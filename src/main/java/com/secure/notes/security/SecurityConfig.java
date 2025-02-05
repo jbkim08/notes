6b5d10b08e6d2a5f04505241a03558aff420e5e3
JB <drv98@naver.com>
2025-02-05 11:14:08 +0900

    시큐리티 설정 변경 CSRF 중지

--- src/main/java/com/secure/notes/security/SecurityConfig.java
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.secure.notes.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,12 +18,9 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests
                 -> requests
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/public/**").permitAll()
                     .anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable); //CSRF 중지
         //http.formLogin(withDefaults());
-        http.sessionManagement(session
-                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
 
 
