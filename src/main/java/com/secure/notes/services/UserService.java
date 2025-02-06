a63cc284ab650c61ef8bed5677c4ba6bbb80b862
JB <drv98@naver.com>
2025-02-06 11:24:46 +0900

    관리자용 유저서비스

---
+++ src/main/java/com/secure/notes/services/UserService.java
@@ -0,0 +1,15 @@
+package com.secure.notes.services;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    //유저의 권한을 업데이트
+    void updateUserRole(Long userId, String roleName);
+    //모든 유저를 가져옴
+    List<User> getAllUsers();
+    //한명의 유저를 가져옴 (UserDTO 객체)
+    UserDTO getUserById(Long id);
+}
