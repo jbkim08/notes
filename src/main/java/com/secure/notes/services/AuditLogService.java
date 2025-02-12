b4c27b3e00b2158e276055b57a424241002934c5
JB <drv98@naver.com>
2025-02-12 11:02:18 +0900

    로그 서비스를 노트 서비스에 적용

--- src/main/java/com/secure/notes/services/AuditLogService.java
+++ src/main/java/com/secure/notes/services/AuditLogService.java
@@ -1,10 +1,18 @@
 package com.secure.notes.services;
 
+import com.secure.notes.models.AuditLog;
 import com.secure.notes.models.Note;
+
+import java.util.List;
 
 public interface AuditLogService {
 
     void logNoteCreation(String username, Note note);
     void logNoteUpdate(String username, Note note);
     void logNoteDeletion(String username, Long noteId);
+
+    //모든 로그를 가져오기
+    List<AuditLog> getAllAuditLogs();
+    //노트 한개에 대한 로그들을 가져오기
+    List<AuditLog> getAuditLogsForNoteId(Long id);
 }
