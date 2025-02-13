8b23ea10f4da34e5b402db81a9e6d3b17cd61535
JB <drv98@naver.com>
2025-02-13 09:43:02 +0900

    관리자 액션 컨트롤러 및 서비스

--- src/main/java/com/secure/notes/services/UserService.java
+++ src/main/java/com/secure/notes/services/UserService.java
@@ -1,6 +1,7 @@
 package com.secure.notes.services;
 
 import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.Role;
 import com.secure.notes.models.User;
 
 import java.util.List;
@@ -14,4 +15,16 @@ public interface UserService {
     UserDTO getUserById(Long id);
     //유저 정보를 가져옴 (유저네임)
     User findByUsername(String username);
+    //유저계정 잠금 상태 변경
+    void updateAccountLockStatus(Long userId, boolean lock);
+    //유저 권한들 가져오기 (DB에 있는 모든 권한들)
+    List<Role> getAllRoles();
+    //계정만료상태 업데이트
+    void updateAccountExpiryStatus(Long userId, boolean expire);
+    //계정사용가능상태 업데이트
+    void updateAccountEnabledStatus(Long userId, boolean enabled);
+    //계정 비번만료상태 업데이트
+    void updateCredentialsExpiryStatus(Long userId, boolean expire);
+    //패스워드 업데이트
+    void updatePassword(Long userId, String password);
 }
--- src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
+++ src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.secure.notes.repositories.RoleRepository;
 import com.secure.notes.repositories.UserRepository;
 import com.secure.notes.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,6 +22,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public void updateUserRole(Long userId, String roleName) {
@@ -50,6 +54,55 @@ public class UserServiceImpl implements UserService {
     public User findByUsername(String username) {
         Optional<User> user = userRepository.findByUserName(username);
         return user.orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
+
+    @Override
+    public void updateAccountLockStatus(Long userId, boolean lock) {
+        User user = userRepository.findById(userId).orElseThrow(()->
+                new RuntimeException("User not found"));
+        user.setAccountNonLocked(!lock); //잠근상태 업데이트
+        userRepository.save(user); //유저 업데이트
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public void updateAccountExpiryStatus(Long userId, boolean expire) {
+        User user = userRepository.findById(userId).orElseThrow(()->
+                new RuntimeException("User not found"));
+        user.setAccountNonExpired(!expire); //계정만료상태 업데이트
+        userRepository.save(user); //유저 업데이트
+    }
+
+    @Override
+    public void updateAccountEnabledStatus(Long userId, boolean enabled) {
+        User user = userRepository.findById(userId).orElseThrow(()->
+                new RuntimeException("User not found"));
+        user.setEnabled(enabled); //계정상태 업데이트
+        userRepository.save(user); //유저 업데이트
+    }
+
+    @Override
+    public void updateCredentialsExpiryStatus(Long userId, boolean expire) {
+        User user = userRepository.findById(userId).orElseThrow(()->
+                new RuntimeException("User not found"));
+        user.setCredentialsNonExpired(!expire); //비번만료상태 업데이트
+        userRepository.save(user); //유저 업데이트
+    }
+
+    @Override
+    public void updatePassword(Long userId, String password) {
+        try {
+            User user = userRepository.findById(userId).orElseThrow(()->
+                    new RuntimeException("User not found"));
+            user.setPassword(passwordEncoder.encode(password)); //비번암호화 해 저장
+            userRepository.save(user); //유저 업데이트
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to update password");
+        }
     }
 
     private UserDTO convertToDto(User user) {
