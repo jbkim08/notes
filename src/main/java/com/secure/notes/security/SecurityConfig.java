5d229b6d50b10148dee6b45574b737c8bad0c80c
KJB <drv98@naver.com>
2025-12-16 11:19:47 +0900

    기본 시큐리티 설정

---
+++ src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.secure.notes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        //모든 요청은 인증된 요청만 가능
+        http.authorizeHttpRequests(
+                (requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+
+        return http.build();
+
+    }
+}
