68f69a87d5e674cbbc744f2995666e015e814aca
KJB <drv98@naver.com>
2025-12-17 10:07:04 +0900

    유저리포

---
+++ src/main/java/com/secure/notes/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.secure.notes.repositories;
+
+import com.secure.notes.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    /* 유저네임으로 찾는 메소드 */
+    Optional<User> findByUsername(String username);
+    /* 유저네임으로 유저가 있는지 확인 */
+    Boolean existsByUsername(String username);
+}
