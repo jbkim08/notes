8ca74cafb95804ff5ea2cde7474b7927bf781806
KJB <drv98@naver.com>
2025-12-17 10:38:41 +0900

    유저 자동 등록

--- src/main/java/com/secure/notes/repositories/UserRepository.java
+++ src/main/java/com/secure/notes/repositories/UserRepository.java
@@ -11,6 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /* 유저네임으로 찾는 메소드 */
     Optional<User> findByUserName(String username);
     /* 유저네임으로 유저가 있는지 확인 */
-    Boolean existsByUsername(String username);
+    Boolean existsByUserName(String username);
 
 }
