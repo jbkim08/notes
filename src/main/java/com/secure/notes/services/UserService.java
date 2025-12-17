1eccf7471209c9ed1be2c01b308ec03a47071a45
KJB <drv98@naver.com>
2025-12-17 11:23:15 +0900

    유저 서비스 구현

---
+++ src/main/java/com/secure/notes/services/UserService.java
@@ -0,0 +1,16 @@
+package com.secure.notes.services;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    //유저의 권한을 수정
+    void updateUserRole(Long userId, String roleName);
+    //모든 유저 가져오기
+    List<User> getAllUsers();
+    //id로 특정 유저를 가져오기
+    UserDTO getUserById(Long id);
+
+}
---
+++ src/main/java/com/secure/notes/services/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.secure.notes.services.impl;
+
+import com.secure.notes.dtos.UserDTO;
+import com.secure.notes.models.AppRole;
+import com.secure.notes.models.Role;
+import com.secure.notes.models.User;
+import com.secure.notes.repositories.RoleRepository;
+import com.secure.notes.repositories.UserRepository;
+import com.secure.notes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void updateUserRole(Long userId, String roleName) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 못찾았습니다."));
+        AppRole appRole = AppRole.valueOf(roleName); // 업데이트 할 권한
+        Role role = roleRepository.findByRoleName(appRole)
+                .orElseThrow(() -> new RuntimeException("권한을 못찾았습니다."));
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow();
+        return convertToDto(user);
+    }
+    //유저DTO로 변환하는 메소드
+    private UserDTO convertToDto(User user) {
+        return new UserDTO(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.isAccountNonLocked(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isEnabled(),
+                user.getCredentialsExpiryDate(),
+                user.getAccountExpiryDate(),
+                user.getTwoFactorSecret(),
+                user.isTwoFactorEnabled(),
+                user.getSignUpMethod(),
+                user.getRole(),
+                user.getCreatedDate(),
+                user.getUpdatedDate()
+        );
+    }
+}
