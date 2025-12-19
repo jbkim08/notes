20e7e913d8bec0a0ad17d033e567c110e7db9d1c
KJB <drv98@naver.com>
2025-12-19 14:27:11 +0900

    오딧로그 컨트롤러

---
+++ src/main/java/com/secure/notes/controllers/AuditLogController.java
@@ -0,0 +1,22 @@
+package com.secure.notes.controllers;
+
+import com.secure.notes.models.AuditLog;
+import com.secure.notes.services.AuditService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/audit")
+public class AuditLogController {
+    @Autowired
+    AuditService auditService;
+
+    @GetMapping
+    public List<AuditLog> getAuditLogs(){
+        return auditService.getAllAuditLogs();
+    }
+}
